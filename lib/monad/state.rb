ca0ba4740c4ee7fd63f20ed794b823ac64645193
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-01-25 09:19:49 -0800

    Implemented StateT. Removed autobinding behavior.

--- lib/monad/state.rb
+++ lib/monad/state.rb
@@ -1,5 +1,18 @@
 require 'monad'
 
+module MonadState
+
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def update(&fn)
+      get.bind { |s| put(fn.call(s)) }
+    end
+  end
+end
+
 class State
   include Monad
   
@@ -10,31 +23,67 @@ class State
   def call(*args)
     @fn.call(*args)
   end
+  
   alias run call
   
   def self.return(val, &fn)
-    m = new { |s| [val, s] }
-    if fn then m.bind(&fn) else m end
+    new { |s| [val, s] }
   end
   
   def bind(&block)
     self.class.new do |s|
-      v, s_ = self.call(s)
-      block.call(v).call(s_)
+      v, s1 = self.call(s)
+      block.call(v).call(s1)
     end
   end
   
+  include MonadState
+  
   def self.get(&fn)
-    m = new { |s| [s, s] }
-    if fn then m.bind(&fn) else m end
+    new { |s| [s, s] }
   end
   
   def self.put(s, &fn)
-    m = new { |_| [nil, s] }
-    if fn then m.bind(&fn) else m end
+    new { |_| [nil, s] }
   end
-  
-  def self.update(&fn)
-    get { |s| put(fn.call(s)) }
+end
+
+def StateT(inner)
+  Class.new do
+    @@inner = inner
+    
+    def initialize(&fn)
+      @fn = fn
+    end
+    
+    def call(*args)
+      @fn.call(*args)
+    end
+    
+    alias run call
+    
+    include Monad
+    
+    def self.return(val)
+      new { |s| @@inner.return([val, s]) }
+    end
+    
+    def bind(&fn)
+      self.class.new do |s|
+        self.call(s).bind do |(v,s1)|
+          fn.call(v).call(s1)
+        end
+      end
+    end
+    
+    include MonadState
+    
+    def self.get(&fn)
+      new { |s| @@inner.return([s, s]) }
+    end
+    
+    def self.put(s, &fn)
+      new { |_| @@inner.return([nil, s]) }
+    end    
   end
 end
