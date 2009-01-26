97ffd59547e4cbbefb1540e52e67c2feec2cdf0a
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-01-25 22:40:05 -0800

    Implemented ReaderT.

--- lib/monad/reader.rb
+++ lib/monad/reader.rb
@@ -1,35 +1,89 @@
 require 'monad'
 
+module MonadReader
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def self.asks(&fn)
+      ask.bind { |r| fn.call(r) }
+    end
+  end
+end
+
 class Reader
-  include Monad
   
   def initialize(&fn)
     @fn = fn
   end
-
+  
   def call(*args)
     @fn.call(*args)
   end
   
   alias run call
   
-  def self.return(val, &fn)
-    m = Reader.new { |e| val }
-    if fn then m.bind(&fn) else m end
+  include Monad
+  
+  def self.return(val)
+    new { |e| val }
   end
-
+  
   def bind(&fn)
-    Reader.new do |e|
-      fn.call(self.call(e)).call(e)
+    self.class.new { |e|
+      fn.call(self.call(e)).call(e) }
+  end
+  
+  include MonadReader
+  
+  def self.ask
+    new { |e| e }
+  end
+  
+  def local(&fn)
+    self.class.new { |e| self.call(fn.call(e)) }
+  end
+end
+
+def ReaderT(inner)
+  Class.new do
+    @@inner = inner
+    
+    def initialize(&fn)
+      @fn = fn
     end
-  end
-
-  def self.ask(&fn)
-    m = Reader.new { |e| e }
-    if fn then m.bind(&fn) else m end
-  end
-
-  def self.asks(&fn)
-    Reader.ask.bind { |r| fn.call(r) }
+    
+    def call(*args)
+      @fn.call(*args)
+    end
+    
+    alias run call
+    
+    include Monad
+    
+    def self.return(val)
+      new { |e| @@inner.return(val) }
+    end
+    
+    def bind(&fn)
+      self.class.new { |e|
+        self.call(e).bind { |val|
+          fn.call(val).call(e) } }
+    end
+    
+    def self.lift(m)
+      new { |e| m }
+    end
+    
+    include MonadReader
+    
+    def self.ask
+      new { |e| @@inner.return(e) }
+    end
+    
+    def local(&fn)
+      self.class.new { |e| self.call(fn.call(e)) }
+    end
   end
 end
