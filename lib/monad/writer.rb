de1c85ed524978bb48c90586c2e16478fcecc477
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-01-25 20:45:20 -0800

    Implemented WriterT.

--- lib/monad/writer.rb
+++ lib/monad/writer.rb
@@ -1,14 +1,15 @@
 require 'monad'
+require 'monoid'
 
 module MonadWriter
   def listens(&fn)
-    bind do |(v, l)|
-      self.class.return(v, fn.call(l))
+    bind do |(val, log)|
+      self.class.return([val, fn.call(log)])
     end
   end
   
   def censor(&fn)
-    bind { |v| self.class.return([v, fn]) }.pass
+    bind { |val| self.class.return([val, fn]) }.pass
   end
 end
 
@@ -16,23 +17,24 @@ def Writer(log_type)
   Class.new do
     @@log_type = log_type
     
-    include Monad
-    include MonadWriter
-    
     attr_reader :value, :log
     
     def initialize(val, log)
       @value, @log = val, log
     end
     
+    include Monad
+    
     def self.return(val)
       new(val, @@log_type.mempty)
     end
     
     def bind(&fn)
-      w1 = fn.call(@value)
-      self.class.new(w1.value, @log.mappend(w1.log))
+      w1 = fn.call(value)
+      self.class.new(w1.value, log.mappend(w1.log))
     end
+    
+    include MonadWriter
     
     def self.tell(log)
       new(nil, log)
@@ -45,6 +47,52 @@ def Writer(log_type)
     
     def listen
       self.class.new([value, log], log)
+    end
+  end
+end
+
+def WriterT(log_type, inner)
+  Class.new do
+    @@log_type = log_type
+    @@inner    = inner
+
+    attr_reader :value
+    
+    def initialize(val)
+      @value = val
+    end
+    
+    include Monad
+    
+    def self.return(val)
+      new(@@inner.return([val, @@log_type.mempty]))
+    end
+    
+    def bind(&fn)
+      self.class.new value.bind { |(a,log)|
+        fn.call(a).value.bind { |(b,log_)|
+          @@inner.return([b, log.mappend(log_)]) } }
+    end
+    
+    def self.lift(m)
+      new m.bind { |val| @@inner.return([val, @@log_type.mempty]) }
+    end
+    
+    include MonadWriter
+    
+    def self.tell(log)
+      new(@@inner.return([nil, log]))
+    end
+    
+    def pass
+      value.bind do |((val,fn), log)|
+        self.class.new(@@inner.return([val, fn.call(log)]))
+      end
+    end
+    
+    def listen
+      self.class.new value.bind { |(val,log)|
+        @@inner.return([[val,log], log]) }
     end
   end
 end
